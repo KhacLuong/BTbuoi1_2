1630798b88a6af4617f36319e2a810e2528406ea
PC <PC@ADMIN>
2022-08-28 17:41:47 +0700

    Add project files.

---
+++ BTbuoi1/Controllers/PloyerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTbuoi1.Controllers
+{
+    public class PloyerController : Controller
+    {
+        // GET: Ployer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
